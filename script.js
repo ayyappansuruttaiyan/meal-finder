e19639f3840ceff3d2f4e1fffe0ddf3c594ac86c
ayyappansuruttaiyan <ayyappan.suruttaiyan@gmail.com>
2023-09-11 18:16:17 +0530

    styles applied

--- script.js
+++ script.js
@@ -1,28 +1,47 @@
 const meals = document.querySelector(".meal");
 const mealsCountEl = document.querySelector(".meals-count");
+const noInputEl = document.querySelector(".no-input");
 const searchEl = document.getElementById("search");
 const submitBtn = document.getElementById("submit");
 const searchHeadingEl = document.querySelector(".search-result-heading");
-
-async function searchMeal(value) {
+const mealsContainerEl = document.querySelector(".meals-container");
+async function searchMeal(e) {
   const userInput = searchEl.value;
-  const response = await fetch(
-    `https://www.themealdb.com/api/json/v1/1/search.php?s=${userInput}`
-  );
+  if (userInput === "") {
+    noInputEl.innerHTML = "No Input found";
+  } else {
+    const response = await fetch(
+      `https://www.themealdb.com/api/json/v1/1/search.php?s=${userInput}`
+    );
 
-  const data = await response.json();
-  const mealsCount = data.meals.length;
-  console.log(mealsCount);
-  searchHeadingEl.innerHTML = `<h3>Dish includes: ${searchEl.value}</h3>
+    const data = await response.json();
+    if (data.meals === null) {
+      noInputEl.innerHTML = "No dish found";
+      mealsContainerEl.innerHTML = "";
+      searchHeadingEl.innerHTML = "";
+      mealsCountEl.innerHTML = "";
+    } else {
+      const mealsCount = data.meals.length;
+      console.log(mealsCount);
+      searchHeadingEl.innerHTML = `<h3>Dish includes: ${searchEl.value}</h3>
 `;
+      mealsCountEl.innerHTML = `<h4>  ${mealsCount} Dishes found </h4>`;
+      mealsContainerEl.innerHTML = data.meals
+        .map(
+          (meal) =>
+            `
+          <div class="meal"> 
+          <img src="${meal.strMealThumb}" alt="${meal.strMeal}" />
+          <h3>${meal.strMeal}</h3> 
+          <button> <a href="">Watch Video<a/></button>
+          </div> 
+          `
+        )
+        .join("");
 
-  mealsCountEl.innerHTML = `<h4>  ${mealsCount} Dishes found </h4>`;
-
-  meals.innerHTML = data.meals.map(
-    (meal) =>
-      `<div> <img width="100px" height="100px"src="${meal.strMealThumb}"</div> <h3>${meal.strMeal}</h3> <button> <a href="">Watch Video<a/></button>`
-  );
-  //   meals.innerHTML = mealsList;
+      noInputEl.innerHTML = "";
+    }
+  }
 }
 
 submitBtn.addEventListener("click", () => {
